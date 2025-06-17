9f49886296fb53b1bd4f2bc302d8e7c06643ff3d
Maarten Behn <maarten.behn@gmail.com>
2025-06-17 16:16:11 +0200

    fix

--- lua/djist/init.lua
+++ lua/djist/init.lua
@@ -17,17 +17,17 @@
 -- along with djist.  If not, see <http://www.gnu.org/licenses/>.
 --
 
-local M = {}
-local nodes = require('djist.nodes')
-local ts_utils = require('nvim-treesitter.ts_utils')
-local bridge = require('djist.pybridge')
+-- local M = {}
+-- local nodes = require('djist.nodes')
+-- local ts_utils = require('nvim-treesitter.ts_utils')
+-- local bridge = require('djist.pybridge')
 
-M.showViewURLS = function ()
-    local view, viewBody = nodes.getViewNodes()
-    print(ts_utils.get_node_text(view)[1])
-    local viewName = ts_utils.get_node_text(view)[1]
-    local sRow, _, _, _ = viewBody:range()
-    bridge.runPython({viewName=viewName, rowNo=sRow})
-end
+-- M.showViewURLS = function ()
+--     local view, viewBody = nodes.getViewNodes()
+--     print(ts_utils.get_node_text(view)[1])
+--     local viewName = ts_utils.get_node_text(view)[1]
+--     local sRow, _, _, _ = viewBody:range()
+--     bridge.runPython({viewName=viewName, rowNo=sRow})
+-- end
 
-return M
+--return M
